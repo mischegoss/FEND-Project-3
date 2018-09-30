f630d6a714623ba0196f89f62fbe50c169a7df83
Tamar Auber <mischegoss@gmail.com>
2018-09-30 15:48:31 -0400

    Added row of enemies, replaced 'if else' for player moves

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-let enemySpeed = 200;
-class Enemy {
+'use strict'
+
+let enemySpeed = 100; // This is stored in a variable to allow easy devlopment of different levels
+class Enemy { // Reconfigured starter code
   constructor(x,y,speed)  {
     this.sprite = 'images/enemy-bug.png';
     this.y = y;
@@ -7,14 +9,7 @@ class Enemy {
     this.speed = Math.random() * 200 + enemySpeed;
   }
 };
-/*var Enemy =
-  function (x, y, speed) {
-    this.sprite = 'images/enemy-bug.png';
-    this.y = y;
-    this.x = x;
-    this.speed = Math.random() * 200 + enemySpeed;
-  };
-*/
+
 Enemy.prototype.update = function (dt) {
   this.x += this.speed * dt;
   if (this.x > 500) {
@@ -22,29 +17,61 @@ Enemy.prototype.update = function (dt) {
   }
 
 };
-
+// This is the provided Enemy render
 Enemy.prototype.render = function() {
  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 class Player {
   constructor() {
-    this.x = 200;
-    this.y = 350;
-    this.sprite = 'images/char-boy.png';
+    this.x = 200;// Sets starting position
+    this.y = 380;//to bottom middle of screen
+    this.sprite = 'images/char-horn-girl.png'; // Sets the Sprite image
+    this.step = 101; // This sets how big the step is so moves one block
+    this.jump = 85;// at a time.
   }
+//This is just using the same method as the provided Enemy method above
  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
  }
  update(dt) {
 
  }
+ // This moves the Player. Method based on Matthew Cranford's tutorial. Replaces 'if else' method.
+handleInput (input) {
+  switch (input) {
+     case 'left':
+         if (this.x > 0) {
+             this.x -= this.step;
+         }
+         break;
+     case 'up':
+         if (this.y > this.jump - 75) { // This adds a bit of padding so the player makes it to the top grass.
+             this.y -= this.jump;
+         }
+         break;
+     case 'right':
+         if (this.x < this.step * 3) {
+             this.x += this.step;
+         }
+         break;
+     case 'down':
+         if (this.y < this.jump * 4) {
+             this.y += this.jump;
+             break;
+         }
+ }
+    }
+
 };
+
+
 const enemy = new Enemy();
-const allEnemies = [new Enemy(-200, 65), new Enemy(-150, 145), new Enemy(-100, 230)];
+// array makes multiple enemies
+const allEnemies = [new Enemy(-200, 65), new Enemy(-150, 145), new Enemy(-100, 230), new Enemy(-50, 310)];
 const player = new Player();
 
-
+// This is the provided Event Listener
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
